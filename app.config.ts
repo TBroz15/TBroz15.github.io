bb7d4a736f0c450bea442c37f01271ba9e61c46f
TBroz15 <nuh@uh.com>
2025-02-16 10:14:27 +0800

    starting over again

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "@solidjs/start/config";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // @ts-expect-error Totally works fine. TS is just shouting false errors
-  vite: { plugins: [tailwindcss()] },
-
   server: {
     baseURL: process.env.BASE_PATH,
     static: true,
